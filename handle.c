21e2979d3ce6440adf86a9dc003d802e134af8bf
Dara <arad1307@utexas.edu>
2014-09-19 15:05:43 -0500

    handle works, but no sig handler for ^c

--- handle.c
+++ handle.c
@@ -17,9 +17,46 @@
  * Finally, loop forever, printing "Still here\n" once every
  * second.
  */
+
+// Dara is driving
 int main(int argc, char **argv)
 {
-  return 0;
+  /* use getpid() system call to find pid
+   *
+   * print pid
+   *
+   * loop continuously, printing "Still here\n" once every second
+   * 
+   * set up a signal handler so that if you hit ctrl-c (^c), it prints nice
+   * try and continues the loop
+   */
+
+   int pid;                       // pid of calling funcion
+   const struct timespec interval = {1};
+
+   pid = getpid();
+   fprintf(stdout, "%d\n", pid);
+   
+   while(1)
+   {
+      fprintf(stdout, "Still here\n");
+      nanosleep(&interval, NULL);
+//      if(argv[0] == '^c')
+//      {
+//         SIGCTRLC(argv[0]);
+//      }
+   }
+   return 0;
 }
+
+/*handler_t *SIGCTRLC(char *cmdline)
+{
+   if(&cmdline == )
+      ssize_t bytes;
+      const int STDOUT = 1;
+      bytes = writes(STDOUT, "Nice try.\n", 10);
+
+   return 0;
+}*/
 
 
